caced6b2c7d8a5c0398c508c4791654883210edd
Chris Carrington <carrington.christopher@gmail.com>
2025-06-14 17:08:46 -0700

    v21

--- cli.ts
+++ cli.ts
@@ -47,7 +47,7 @@ class Build {
         start: 'vinxi start',
       },
       devDependencies: {
-        '@acets-team/ace': '^0.0.20',
+        '@acets-team/ace': '^0.0.21',
         '@solidjs/meta': '^0.29.4',
         '@solidjs/router': '^0.15.3',
         '@solidjs/start': '^1.1.4',
