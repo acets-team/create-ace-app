1794e70507e49c9052478104c203d9dbce723ddd
Chris Carrington <carrington.christopher@gmail.com>
2025-07-09 16:13:22 -0700

    valibotString2Int > valibotString2Number

--- src/api/apiFortune.ts
+++ src/api/apiFortune.ts
@@ -3,11 +3,11 @@ import { object } from 'valibot'
 import { holdUp } from '@ace/holdUp'
 import { fortunes } from '@src/lib/vars'
 import { valibotParams } from '@ace/valibotParams'
-import { valibotString2Int } from '@ace/valibotString2Int'
+import { valibotString2Number } from '@ace/valibotString2Number'
 
 
 export const GET = new API('/api/fortune/:id', 'apiFortune')
-  .pathParams(valibotParams(object({ id: valibotString2Int() })))
+  .pathParams(valibotParams(object({ id: valibotString2Number() })))
   .resolve(async (be) => {
     await holdUp()
     return be.success({ id: be.pathParams.id, fortune: fortunes[be.pathParams.id] })
