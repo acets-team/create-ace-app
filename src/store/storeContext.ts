e7593a5c9d0e1e23a95defac93277eb04618a2b6
Chris Carrington <carrington.christopher@gmail.com>
2025-10-18 20:51:00 -0700

    0.3.1

--- src/store/storeContext.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'solid-js'
-import type { atoms } from '@src/store/atoms'
-import type { BaseStoreContext } from '@ace/types'
-
-
-export const StoreContext = createContext<BaseStoreContext<typeof atoms>>()
