7ed90535592933415f5762e943b148b2d68586aa
Chris Carrington <carrington.christopher@gmail.com>
2025-10-23 19:46:25 -0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -2,6 +2,6 @@
 
 import { addOfflineSupport } from '../.ace/addOfflineSupport.js'
 
-const packageDotJsonVersion = '0.4.1'
+const packageDotJsonVersion = '0.4.2'
 
 addOfflineSupport({ cacheName: `offline-cache-v-${packageDotJsonVersion}` })
