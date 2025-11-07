2d8615ed66ee822fa9c61f3cc03da4bbbaf4d3c7
Chris Carrington <carrington.christopher@gmail.com>
2025-11-06 21:01:26 -0800

    0.7.4 bump

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@
 
 import { swAddOffLineSupport } from './.ace/swAddOffLineSupport.js'
 
-const packageDotJsonVersion = '0.7.3'
+const packageDotJsonVersion = '0.7.4'
 
 swAddOffLineSupport({
   installUrls: ['/', '/chat', '/fortunes'],
