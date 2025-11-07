8ffc30c5fab125ee7b62a5562b2511652390ee82
Chris Carrington <carrington.christopher@gmail.com>
2025-11-06 23:33:38 -0800

    inline sw

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,4 +1,6 @@
 // @refresh reload
+import swRegister from '@ace/swRegister?raw'
+import swStyles from '@ace/sw.styles.css?raw'
 import { createHandler, StartServer } from '@solidjs/start/server'
 
 export default createHandler(() => (
@@ -7,14 +9,11 @@ export default createHandler(() => (
       <html lang="en">
         <head>
           <meta charset="utf-8" />
+          <style>{swStyles}</style>
           <link rel="icon" href="/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
-          <link href="/.ace/sw.styles.css" rel="stylesheet" />
-
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta name="description" content="Create Ace App is a showcase for what is possible w/ Ace!" />
-
-          {/* disables manual user zooming 🚨 browsers may ignore */}
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
           {/* enhance how the site looks when added to an iOS device's home screen */}
           <meta name="apple-mobile-web-app-capable" content="yes" /> {/* tells iOS Safari that the web application should be run in full-screen mode without the standard browser chrome (address bar, toolbar) when launched from a home screen icon */}
@@ -24,7 +23,7 @@ export default createHandler(() => (
         </head>
         <body>
           <div id="app">{children}</div>
-          <script src="/.ace/swRegister.js"></script>
+          <script>{swRegister}</script>
           {scripts}
         </body>
       </html>
